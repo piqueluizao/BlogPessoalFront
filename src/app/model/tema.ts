dac1b725a8eea8a211bbb006a9e02548959be0d0
Luiza <lujoseds@gmail.com>
2022-07-09 21:41:23 -0300

    consumo de API model e service

---
+++ src/app/model/tema.ts
@@ -0,0 +1,9 @@
+import { Postagem } from "./postagem"
+
+
+export class Tema{
+    public id: number
+    public descricao: string
+    public postagem: Postagem[]
+
+}
---
+++ src/app/model/usuarioLogin.ts
@@ -0,0 +1,10 @@
+export class UsuarioLogin{
+    public id: number
+    public nome: string
+    public usuario: string
+    public foto: string
+    public biografia: string
+    public senha: string
+    public token: string
+    public tipo: string
+}
