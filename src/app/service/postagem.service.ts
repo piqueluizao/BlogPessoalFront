02701ce0618aa7faf50380dba278dcd9b8f683c5
Luiza <lujoseds@gmail.com>
2022-07-12 20:52:56 -0300

    task

---
+++ src/app/service/postagem.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostagemService {
+
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization',environment.token)
+  }
+}
