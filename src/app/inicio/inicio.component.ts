d0824281c7cf9d92a6d71674fd55e2925c1bf884
Luiza <lujoseds@gmail.com>
2022-07-13 21:41:05 -0300

    crud

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { Usuario } from '../model/Usuario';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,13 +14,49 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  postagem: Postagem = new Postagem
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
+
+  usuario: Usuario = new Usuario()
+  idUsuario = environment.id
+
+  constructor(private router: Router, private postagemService: PostagemService, private temaService: TemaService) { }
 
   ngOnInit() {
     if(environment.token == ''){
       alert('Sua seção espirou, faça o login novamente')
       this.router.navigate(['/entrar'])
     }
+    this.getAllTemas()
+  }
+
+  getAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema)=>{
+      this.tema = resp
+    })
+  }
+
+  publicar(){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.usuario.id = this.idUsuario
+    this.postagem.usuario = this.usuario
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+      this.postagem = resp
+      alert('Postagem realizada com sucesso!')
+      this.postagem = new Postagem()
+    })
   }
 
 }
--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
 
 @Injectable({
   providedIn: 'root'
@@ -12,4 +14,14 @@ export class PostagemService {
   token = {
     headers: new HttpHeaders().set('Authorization',environment.token)
   }
+
+  getAllPostagens(): Observable<Postagem[]>{
+    return this.http.get<Postagem[]>('http://localhost:8080/postagens', this.token)
+
+  }
+
+  postPostagem(postagem: Postagem) : Observable<Postagem>{
+    return this.http.post<Postagem>('http://localhost:8080/postagens', postagem, this.token)
+  }
+
 }
